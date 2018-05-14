4cba9b71a072a64223755899e5946d7e8c342f03
power80203 <power80203@gmail.com>
2018-05-14 11:12:24 +0800

    renew config

--- 2-MLP_ANN_Training/config.py
+++ 2-MLP_ANN_Training/config.py
@@ -3,3 +3,8 @@
 #########################################################
 
 path_local = r"D:\Users\2063\Dropbox\2-Self Training\2-Programming\Python\Python_Project_Kaggle\2-MLP_ANN_Training\data\test.csv"
+
+
+#########################################################
+#ANN parameter#
+#########################################################
